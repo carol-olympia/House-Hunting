e5cc7d8ae7f17c4a81836413c009e33b801ef263
mark kinai <mark1cyfred1@gmail.com>
2023-09-28 15:46:26 +0300

    home page click page

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html>
-<head></head>
+<head>
+</head>
 <body>
     <header>
         <nav>
             <ul>
                 <li>HOME</li>
-                <LI>ABOUT</LI>
-                <LI>SERVICES</LI>
-                <LI>CONTACTS</LI>
-                <a href="sign-up.php"></a> <LI>  GET STARTED</LI>
-            
+                <li>ABOUT</li>
+                <li>SERVICES</li>
+                <li>CONTACTS</li>
+                <li><a href="sign-up.php">GET STARTED</a></li>
             </ul>
         </nav>
         <hr>
@@ -18,24 +18,23 @@
     </header>
     <section>
         <h3>ABOUT</h3>
-        <P>This website aims to revolutionize the process of finding and selecting houses;
+        <p>This website aims to revolutionize the process of finding and selecting houses;
             it will provide users with an interface and powerful search functionalities to streamline the house hunting project/journey
-        and offer different features designed to enhance user experience and facilitate informed decision making.<br>
-            One will be able to create personalized profiles allowing You to save preferences, track favorite properties and receive tailored recommendation based on your specific requirements.</P>
-        <p> A search engine will be incorporated to enable everyone to search based on location, price, property features, neighborhood information and other necessities; the website will also facilitate communication between users and real estate agents and landlords/property owners with maximum authenticity allowing face locks and high password configuration.<br>
-            By leveraging advanced technologies, data driven insights and useful friendly features, the website will empower individuals to find their dream homes efficiently and confidentially.
-            </P>
-            <img scr="apartments.jpg" width="500" height="400">
+            and offer different features designed to enhance user experience and facilitate informed decision making.<br>
+            One will be able to create personalized profiles allowing You to save preferences, track favorite properties and receive tailored recommendation based on your specific requirements.</p>
+        <p>A search engine will be incorporated to enable everyone to search based on location, price, property features, neighborhood information and other necessities; the website will also facilitate communication between users and real estate agents and landlords/property owners with maximum authenticity allowing face locks and high password configuration.<br>
+            By leveraging advanced technologies, data-driven insights and useful friendly features, the website will empower individuals to find their dream homes efficiently and confidentially.
+        </p>
+        <img src="apartments.jpg" width="500" height="400">
     </section>
-    </body>
-    <body>
+    <section>
         <h3>SERVICES</h3>
-    </body>
+    </section>
     <footer>
         <h4>FIND US</h4>
         <ul>
-            <li>Whatsspp- 0723464616</li>
-            <li>Instagram- cosyhomes</li>
+            <li>WhatsApp - 0723464616</li>
+            <li>Instagram - cosyhomes</li>
         </ul>
     </footer>
 </body>
