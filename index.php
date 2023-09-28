d548a321884330cdd5c09052565f58dd02b410f5
KELVIN KINYANJUI <carol-olympia>
2023-09-28 15:43:18 +0300

    homepage

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li>HOME</li>
+                <LI>ABOUT</LI>
+                <LI>SERVICES</LI>
+                <LI>CONTACTS</LI>
+                <a href="sign-up.php"></a> <LI>  GET STARTED</LI>
+            
+            </ul>
+        </nav>
+        <hr>
+        <h1>COSY HOMES</h1>
+    </header>
+    <section>
+        <h3>ABOUT</h3>
+        <P>This website aims to revolutionize the process of finding and selecting houses;
+            it will provide users with an interface and powerful search functionalities to streamline the house hunting project/journey
+        and offer different features designed to enhance user experience and facilitate informed decision making.<br>
+            One will be able to create personalized profiles allowing You to save preferences, track favorite properties and receive tailored recommendation based on your specific requirements.</P>
+        <p> A search engine will be incorporated to enable everyone to search based on location, price, property features, neighborhood information and other necessities; the website will also facilitate communication between users and real estate agents and landlords/property owners with maximum authenticity allowing face locks and high password configuration.<br>
+            By leveraging advanced technologies, data driven insights and useful friendly features, the website will empower individuals to find their dream homes efficiently and confidentially.
+            </P>
+            <img scr="apartments.jpg" width="500" height="400">
+    </section>
+    </body>
+    <body>
+        <h3>SERVICES</h3>
+    </body>
+    <footer>
+        <h4>FIND US</h4>
+        <ul>
+            <li>Whatsspp- 0723464616</li>
+            <li>Instagram- cosyhomes</li>
+        </ul>
+    </footer>
+</body>
+</html>
