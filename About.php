63de55b8492c9d4ac3bc75ddb315673c96f1ce48
mark kinai <mark1cyfred1@gmail.com>
2023-10-04 16:58:36 +0300

    about

--- About.php
+++ About.php
@@ -1,29 +1,34 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 
-            <link href="assets/img/Real Estate Logo.png" rel="icon">
-            <!-- CSS FILES -->
-            <link href="assets/css/bootstrap.min.css" rel="stylesheet">
 
-            <link href="icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/img/Real Estate Logo.png" rel="icon">
+    <!-- CSS FILES -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
 
-            <link href="assets/css/house-hunt-header.css" rel="stylesheet">
+    <link href="icons/bootstrap-icons.css" rel="stylesheet">
 
-            <link href="assets/css/homepage.css" rel="stylesheet">
+    <link href="assets/css/house-hunt-header.css" rel="stylesheet">
 
-            <link href="assets/css/belowheader.css" rel="stylesheet">
+    <link href="assets/css/homepage.css" rel="stylesheet">
 
-            <link href="assets/css/footer.css" rel="stylesheet">
+    <link href="assets/css/belowheader.css" rel="stylesheet">
 
-        </head>
+    <link href="assets/css/about.css" rel="stylesheet">
+
+    <link href="assets/css/footer.css" rel="stylesheet">
+
+</head>
 
 
 
 
 <body style="background-color:white;">
-<?php include 'include/header.php'; ?>
+    <?php include 'include/header.php'; ?>
 
 
     <!-- About Us Section -->
@@ -52,10 +57,124 @@
                     </p>
                 </div>
 
-        </div> 
-       </section>
+            </div>
+    </section>
 
-  
+    <!-- About Us Section -->
+    <section class="about-us">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="text-center">
+                        <h2 class="text-secondary mb-4">About Us</h2>
+                    </div>
+                    <p>
+                        Welcome to House Hunt, your trusted platform for finding the perfect property. We strive to provide exceptional service and assist you in every step of your real estate journey.<br>
+                        Our mission is to simplify the process of buying, selling, and renting properties, ensuring a seamless and enjoyable experience for our clients; this is enabled by our team of highly qualified and experienced professionals who are dedicated to providing the best service to our clients through our website and mobile app which help in navigation and keying in of data.
+                    </p>
+                </div>
+                <div class="col-lg-6">
+                    <div class="text-center">
+                        <img src="assets/img/banner.jpeg" alt="House Image" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Custom Section -->
+    <section class="custom-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="text-center">
+                        <img src="assets/img/maxresdefault.jpg" alt="House Image" class="img-fluid">
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="text-center">
+                        <h2 class="text-secondary mb-4">Who We Are </h2>
+                        <p>
+                            Welcome to House Hunt, your trusted platform for finding the perfect property. We strive to provide exceptional service and assist you in every step of your real estate journey.<br>
+                            Our mission is to simplify the process of buying, selling, and renting properties, ensuring a seamless and enjoyable experience for our clients; this is enabled by our team of highly qualified and experienced professionals who are dedicated to providing the best service to our clients through our website and mobile app which help in navigation and keying in of data.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section id="team" class="team">
+
+        <!--  Section Title -->
+        <div class="container section-title" data-aos="fade-up">
+            <h2>Team</h2>
+            <p>Our team is made up of highly qualified and experienced professionals who are dedicated to providing the best service to our clients through our website and mobile app which help in navigation and keying in of data to ensure that you get the best property that suits your needs seamlessly.</p>
+        </div>
+
+        <div class="container">
+
+            <div class="row gy-5">
+
+                <div class="col-lg-4 col-md-6 member" data-aos="fade-up" data-aos-delay="100">
+                    <div class="member-img">
+                        <img src="assets/img/banner.jpeg" class="img-fluid" alt="">
+                        <div style="color: #f4f4f4;" class="social">
+                            <a href="#"><i class="bi bi-twitter"></i></a>
+                            <a href="#"><i class="bi bi-facebook"></i></a>
+                            <a href="#"><i class="bi bi-instagram"></i></a>
+                            <a href="#"><i class="bi bi-linkedin"></i></a>
+                        </div>
+                    </div>
+                    <div class="member-info text-center">
+                        <h4>Carol Njenga</h4>
+                        <span>Chief Executive Officer</span>
+                        <p>Mark Kinai, our skilled CTO, leads our technology endeavors, steering innovation and ensuring technological excellence. With strategic planning and expertise, he propels us towards a successful, forward-thinking future</p>
+                    </div>
+                </div><!-- End Team Member -->
+
+                <div class="col-lg-4 col-md-6 member" data-aos="fade-up" data-aos-delay="200">
+                    <div class="member-img">
+                        <img src="assets/img/banner.jpeg" class="img-fluid" alt="">
+                        <div style="color: #f4f4f4;" class="social">
+                            <a href="#"><i class="bi bi-twitter"></i></a>
+                            <a href="#"><i class="bi bi-facebook"></i></a>
+                            <a href="#"><i class="bi bi-instagram"></i></a>
+                            <a href="#"><i class="bi bi-linkedin"></i></a>
+                        </div>
+                    </div>
+                    <div class="member-info text-center">
+                        <h4>Carol Njenga</h4>
+                        <span>Product Manager</span>
+                        <p>Mark Kinai, our skilled CTO, leads our technology endeavors, steering innovation and ensuring technological excellence. With strategic planning and expertise, he propels us towards a successful, forward-thinking future</p>
+                    </div>
+                </div><!-- End Team Member -->
+
+                <div class="col-lg-4 col-md-6 member" data-aos="fade-up" data-aos-delay="300">
+                    <div class="member-img">
+                        <img src="assets/img/hero-bg.jpg" class="img-fluid" alt="">
+                        <div style="color: #f4f4f4;" class="social">
+                            <a href="#"><i class="bi bi-twitter"></i></a>
+                            <a href="#"><i class="bi bi-facebook"></i></a>
+                            <a href="#"><i class="bi bi-instagram"></i></a>
+                            <a href="#"><i class="bi bi-linkedin"></i></a>
+                        </div>
+                    </div>
+                    <div class="member-info text-center">
+                        <h4>Mark-Kinai</h4>
+                        <span>CTO</span>
+                        <p>Mark Kinai, our skilled CTO, leads our technology endeavors, steering innovation and ensuring technological excellence. With strategic planning and expertise, he propels us towards a successful, forward-thinking future.</p>
+                    </div>
+                </div><!-- End Team Member -->
+
+
+            </div>
+
+        </div>
+
+    </section><!-- End Team Section -->
+
 
     <!-- Back to Top Button -->
     <div class="back-to-top" id="backToTop">
@@ -63,12 +182,14 @@
     </div>
 
     <!-- Footer -->
-    <footer>
-      <!-- Your mistake  </?include 'include/footer.php'; ?> ------>
-      <!----- correct way to include footer.php ------>
-        <?php include 'include/footer.php'; ?>
-    </footer>
-    
+    <!-- Your mistake  </?include 'include/footer.php'; ?> ------>
+    <!----- correct way to include footer.php ------>
+    <?php include 'include/footer.php'; ?>
+    </>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <!-- Add your existing script imports and custom scripts here -->
 </body>
+
 </html>
