4e6db1b2f2f6ad053a7176347f69d20e1d8227c2
mark kinai <mark1cyfred1@gmail.com>
2023-10-04 15:29:12 +0300

    footer

--- About.php
+++ About.php
@@ -64,7 +64,9 @@
 
     <!-- Footer -->
     <footer>
-        </?include 'include/footer.php'; ?>
+      <!-- Your mistake  </?include 'include/footer.php'; ?> ------>
+      <!----- correct way to include footer.php ------>
+        <?php include 'include/footer.php'; ?>
     </footer>
     
     <!-- Add your existing script imports and custom scripts here -->
